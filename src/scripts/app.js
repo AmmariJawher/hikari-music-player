5ebc44cd605041ccf1d9af6df836e56d99ddca75
AmmariJawher <jawher.ammari@tutanota.com>
2019-08-17 15:30:49 +0100

    Build simple music player

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,42 @@
-console.log("hello"
-+ "my"
-+ "world")
+var music = document.querySelector('audio')
+var progressBar = document.querySelector('#progress-bar')
+var playButton = document.querySelector('.play-button')
+var pauseButton = document.querySelector('.pause-button')
+var volumeController = document.querySelector('#volume')
 
+function playMusic (audio) {
+  audio.play();
+  console.log('test-1-pass'); 
+}
+function  pauseMusic (audio) {
+  audio.pause();
+  console.log('test-2-pass'); 
+}
+
+function updateProgressBar (progressBar, player) {
+  var currentTime = player.currentTime
+  var duration = player.duration
+  var currentProgress = currentTime / duration
+  progressBar.value = currentProgress
+}
+
+function updateAudioVolume (controller, music) {
+  var value = controller.value
+  music.volume = value
+}
+
+window.setInterval(function(){
+  updateProgressBar(progressBar, music)
+}, 500);
+
+// Event Listeners
+playButton.addEventListener('click', function () {
+  playMusic(music)
+})
+pauseButton.addEventListener('click', function () {
+  pauseMusic(music)
+})
+
+volumeController.addEventListener('change', function () {
+  updateAudioVolume(volumeController, music)
+})
