ebc63b118e081fd23fb3d027b37ae50810b47d17
AmmariJawher <jawher.ammari@tutanota.com>
2019-08-17 17:16:01 +0100

    add next and previous buttons

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,40 @@
 var music = document.querySelector('audio')
+var artistName = document.getElementsByClassName('song-artist')
+var songName = document.getElementsByClassName('song-title')
+var musicCover = document.getElementsByClassName('song-cover')
+
+// Controle
 var progressBar = document.querySelector('#progress-bar')
 var playButton = document.querySelector('.play-button')
 var pauseButton = document.querySelector('.pause-button')
 var volumeController = document.querySelector('#volume')
+var nextButton = document.querySelector('.next-button')
+var previousButton = document.querySelector('.previous-button')
+var songNumber = 0
 
+// data
+var musicList = [
+  {
+    artist: 'David Bowie',
+    song: 'Modern Love',
+    source: './assets/audio/David Bowie - Modern Love.mp3',
+    cover: './assets/img/David Bowie - Modern Love.jpg'
+  },
+  {
+    artist: 'Wham!',
+    song: 'Everything She Wants',
+    source: './assets/audio/Wham! - Everything She Wants.mp3',
+    cover: './assets/img/Wham! - Everything She Wants.jpg'
+  },
+  {
+    artist: 'Persona 5',
+    song: 'Last Surprise',
+    source: './assets/audio/Persona 5 - Last Surprise.mp3',
+    cover: './assets/img/Persona 5 - Last Surprise.jpg'
+  }
+]
+
+// Functions
 function playMusic (audio) {
   audio.play();
   console.log('test-1-pass'); 
@@ -29,6 +60,13 @@ window.setInterval(function(){
   updateProgressBar(progressBar, music)
 }, 500);
 
+function updateSong(musicList, currentSong, artistName, songName, musicCover) {
+  artistName.innerHtml = musicList[songNumber].artist
+  songName.innerHtml = musicList[songNumber].song
+  currentSong.src = musicList[songNumber].source
+  musicCover.src = musicList[songNumber].cover
+}
+
 // Event Listeners
 playButton.addEventListener('click', function () {
   playMusic(music)
@@ -40,3 +78,17 @@ pauseButton.addEventListener('click', function () {
 volumeController.addEventListener('change', function () {
   updateAudioVolume(volumeController, music)
 })
+
+nextButton.addEventListener('click', function () {
+  ++songNumber
+  updateSong(musicList, music, artistName, songName, musicCover)
+  playMusic(music)
+})
+
+previousButton.addEventListener('click', function () {
+  --songNumber
+  updateSong(musicList, music, artistName, songName, musicCover)
+  playMusic(music)
+})
+
+updateSong(musicList, music, artistName, songName, musicCover)
